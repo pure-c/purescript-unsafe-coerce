afb95daeeeac802b2f9694d18684c009c685c2d7
Felix Schlitter <felix@sylo.io>
2018-09-14 17:44:47 +1200

    Add C FFI

---
+++ src/Unsafe/Coerce.c
@@ -0,0 +1,5 @@
+#include <purescript.h>
+
+PURS_FFI_FUNC_1(Unsafe_Coerce_unsafeCoerce, x, {
+	return x;
+});
---
+++ src/Unsafe/Coerce.h
@@ -0,0 +1,8 @@
+#ifndef Unsafe_Coerce_H
+#define Unsafe_Coerce_H
+
+#include <purescript.h>
+
+PURS_FFI_EXPORT(Unsafe_Coerce_unsafeCoerce);
+
+#endif // Unsafe_Coerce_H
